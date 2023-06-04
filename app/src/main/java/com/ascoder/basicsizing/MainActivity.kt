0b53713588fac1e54f9803586a755e36a707896b
Arfan Siregar <arfan.siregar@kpvendor.id>
2023-06-04 17:12:27 +0700

    - Create function row and colum for displaying word - Using basic sizing

--- app/src/main/java/com/ascoder/basicsizing/MainActivity.kt
+++ app/src/main/java/com/ascoder/basicsizing/MainActivity.kt
@@ -11,33 +11,66 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.ascoder.basicsizing.ui.theme.BasicSizingTheme
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BasicSizingTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+            Column(
+                modifier = Modifier
+                    .fillMaxSize(0.7f)
+                    .background(color = Color.Green),
+                verticalArrangement = Arrangement.SpaceAround,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Columns(name = "Arfan")
+                Rows(name = "Arfan")
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Columns(name: String) {
+    Column(
+        verticalArrangement = Arrangement.SpaceAround,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Hello")
+        Text(text = "world!")
+        Text(text = "Welcome $name!")
+    }
+}
+
+@Composable
+fun Rows(name: String) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceAround,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(text = "Hello")
+        Text(text = "world!")
+        Text(text = "Welcome $name!")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    BasicSizingTheme {
-        Greeting("Android")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Green),
+        verticalArrangement = Arrangement.SpaceAround,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Columns(name = "Arfan")
+        Rows(name = "Arfan")
     }
 }
